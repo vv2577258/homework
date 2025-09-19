1477198df8ad1d052fd5cdc88e340de977b8a636
DeveloperViktorK <developerviktork@ya.ru>
2025-09-19 23:08:29 +0300

    feat(Calculator): currency calc

--- 1-converted/main.go
+++ 1-converted/main.go
@@ -1,7 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USDtoEUR = 0.849
 	const USDtoRUB = 83.17
 	const EURtoRUB = USDtoRUB / USDtoEUR
+
+	amount := inputAmount()
+	fmt.Printf("Вы ввели  %f", amount)
+}
+
+func inputAmount() float64 {
+	fmt.Printf("Введите сумму")
+	var amount float64
+	fmt.Scan(&amount)
+
+	return amount
+}
+
+func convertAmount(amount float64, currencyFrom string, currencyTo string) float64 {
+	var convertedAmount float64
+
+	return convertedAmount
 }
